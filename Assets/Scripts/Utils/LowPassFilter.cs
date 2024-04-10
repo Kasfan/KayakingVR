30bab5d477dcae78afdaac4a770738a57dd48979
Denis Slabko <debug.start@gmail.com>
2024-04-10 03:39:46 +0300

    added paddle movement and effects

---
+++ Assets/Scripts/Utils/LowPassFilter.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace KayakingVR.Utils
+{
+    /// <summary>
+    /// Lazy version on "Low pass filter for Vector3"
+    /// </summary>
+    public class LowPassFilter
+    {
+        private float smoothingFactor;
+        private Vector3 filteredValue;
+
+        public LowPassFilter(float smoothingFactor)
+        {
+            this.smoothingFactor = smoothingFactor;
+        }
+
+        public Vector3 Filter(Vector3 rawValue)
+        {
+            filteredValue = Vector3.Lerp(filteredValue, rawValue, smoothingFactor);
+            return filteredValue;
+        }
+
+        public void Reset(Vector3 initialValue)
+        {
+            filteredValue = initialValue;
+        }
+    }
+}
