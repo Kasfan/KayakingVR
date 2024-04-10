a1eb2bb07046cefcc821eee68a0e2438c73b3e8e
Denis Slabko <debug.start@gmail.com>
2024-04-10 17:50:01 +0300

    Feature/kayak mechanics (#3)
    * added water fx
    
    * cleanup
    
    * added teleportation in and out of kayak
    
    * added paddle movement and effects
    
    * added kayak paddling mechanics
    
    * cleanup

---
+++ Assets/Scripts/PaddleBladePropulsionController.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+
+namespace KayakingVR
+{
+    [AddComponentMenu("KayakingVR/PaddleBladePropulsionController")]
+    public class PaddleBladePropulsionController: MonoBehaviour
+    {        
+        [SerializeField] 
+        [Tooltip("Trigger collider of the water")]
+        private Collider waterTrigger;
+
+        [SerializeField] 
+        [Tooltip("Multiplier of a drag force that the paddle blade moves through the water")]
+        private float dragForce = 50f;
+
+        [SerializeField]
+        [Tooltip("Rigidbody of the kayak. The script uses force, so the 'kayakBody' can't be kinematic")]
+        private Rigidbody kayakBody;
+
+        private Vector3 movementCounter = Vector3.zero;
+        private Vector3 prevPosition;
+
+        private void Awake()
+        {
+            prevPosition = transform.position;
+        }
+
+        private void Update()
+        {
+            var currentPosition = transform.position;
+            movementCounter += currentPosition - prevPosition;
+            prevPosition = currentPosition;
+
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if(other!=waterTrigger)
+                return;
+            
+            movementCounter = Vector3.zero;
+        }
+
+        private Vector3 GetWaterLevelPosition()
+        {
+            var contactPos = transform.position;
+            contactPos.y = waterTrigger.transform.position.y;
+            return contactPos;
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+            if(other!=waterTrigger)
+                return;
+            
+            ApplyForce(movementCounter);
+            movementCounter = Vector3.zero; // reset the counter
+        }
+
+        private void ApplyForce(Vector3 movement)
+        {
+            var propulsionForce = movement * dragForce;
+            
+            // invert force to achieve - blade moves left, kayak moves right
+            propulsionForce.x = -1f * propulsionForce.x;
+            propulsionForce.z = -1f * propulsionForce.z;
+            propulsionForce.y = 0; // ignore up vector, because the kayak moves in a plane space
+            
+            kayakBody.AddForceAtPosition(propulsionForce, GetWaterLevelPosition());
+            //Debug.Log($"Force: {propulsionForce}");
+        }
+        
+        private void OnTriggerExit(Collider other)
+        {            
+            if(other!=waterTrigger)
+                return;
+
+        }
+    }
+}
---
+++ Assets/Scripts/PaddleBladeWaterFx.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace KayakingVR
+{
+    [AddComponentMenu("KayakingVR/PaddleBladeWaterFx")]
+    public class PaddleBladeWaterFx: MonoBehaviour
+    {
+        [SerializeField] 
+        [Tooltip("Trigger collider of the water")]
+        private Collider waterTrigger;
+        
+        [SerializeField] 
+        [Tooltip("Splash effect when the padded touches the water")]
+        private ParticleSystem splashFx;
+        
+        [SerializeField]
+        [Tooltip("Audio splash effect when the padded touches the water")]
+        private AudioSource splashAudioFx;
+        
+        [SerializeField]
+        [Tooltip("Relation between hit force of the blade on the water and size of the splash fx")]
+        public AnimationCurve splashForce =  AnimationCurve.Linear(0, 0, 10, 1);
+
+        [SerializeField] 
+        [Tooltip("Water foam effect when the padded is in the water")]
+        private ParticleSystem foamFx;
+
+        private float velocity3dFiltered;
+        private Vector3 prevPosition;
+        private Transform mTransform;
+        
+        private void Awake()
+        {
+            mTransform = transform;
+            prevPosition = mTransform.position;
+            
+            if(!waterTrigger.isTrigger)
+                Debug.LogError("Water collider must be trigger");
+            
+            foamFx.Stop();
+        }
+
+        private void Update()
+        {
+            // calculate velocity of the blade
+            var movement = Vector3.Distance(mTransform.position, prevPosition);
+            var velocity3dRaw = movement / Time.deltaTime;
+            
+            // to avoid issues when the movement changes drastically, smooth it
+            velocity3dFiltered = Mathf.Lerp(velocity3dFiltered, velocity3dRaw, 0.3f);
+            
+            prevPosition = mTransform.position;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if(other!=waterTrigger)
+                return;
+
+            OnTriggerStay(other);   //update position of the effects
+
+            foamFx.Play();
+            var size = splashForce.Evaluate(velocity3dFiltered);
+            
+            // calculate common parameters for tha particles
+            var particle = new ParticleSystem.EmitParams()
+            {
+                position = GetWaterLevelPosition(),
+            };
+            var particleSize = Mathf.Lerp(splashFx.main.startSize.constantMin, splashFx.main.startSize.constantMax,
+                size);
+            
+            // emit 5 particles with some randomness to them
+            for (int i = 0; i < 6; i++) 
+            {
+                particle.startSize = particleSize * Random.Range(0.5f, 1.5f);
+                particle.rotation3D = Random.onUnitSphere * 45f;
+                splashFx.Emit(particle,1);
+            }
+                
+            splashAudioFx.PlayOneShot(splashAudioFx.clip, size);
+            //Debug.Log($"size: {size}; velocity: {velocity3dFiltered};");
+        }
+
+        private Vector3 GetWaterLevelPosition()
+        {
+            var contactPos = mTransform.position;
+            contactPos.y = waterTrigger.transform.position.y;
+            return contactPos;
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+            if(other!=waterTrigger)
+                return;
+
+            foamFx.transform.position = GetWaterLevelPosition();
+        }
+
+        private void OnTriggerExit(Collider other)
+        {            
+            if(other!=waterTrigger)
+                return;
+
+            foamFx.Stop();
+        }
+    }
+}
